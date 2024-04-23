8f178d4acdf58546c822b05c641288f5c80d63f4
Harper Nordin <harper.nordin@gmail.com>
2024-04-23 16:07:56 -0700

    Made all stat commands work, we are chilling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import random
 import time
 import dnd_char as dd
@@ -8,11 +9,10 @@ file = open('key.txt', 'r')
 MY_TOKEN = file.read()
 file.close()
 
-intents = discord.Intents.default()
-intents.message_content = True
-
+intents = discord.Intents.all()
 
 client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 characters = []
 character_names = []
@@ -23,161 +23,15 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
 
 
-@client.event
-async def on_message(message):
-    text = 'No message was generated'
-    contents = message.content
-
-    if message.author == client.user:
-        return
-
-    if contents.startswith('!hello'):
-        await message.channel.send('Hello!')
-
-    if contents.startswith('!new_character'):
-        text = new_character(message)
-        await message.channel.send(text)
-
-    if contents.startswith('!roll'):
-        person = message.author
-        words = message.content.lower().split(' ')
-
-        multiplicity = int(words[1].split('d')[0])
-        die = int(words[1].split('d')[1])
-        d_max = max(die, 1)
-        d_min = min(die, 1)
-
-        results = []
-
-        await message.channel.send(f'Rolling {words[1]}')
-
-        for i in range(multiplicity):
-            roll = random.randint(d_min, d_max)
-            results.append(roll)
-            await message.channel.send(f'{roll}')
-            time.sleep(2.4)
-
-        if message.content.count('drop') == 1:
-            drop = int(message.content.split(' ')[-1])
-            results.sort()
-            if drop < 0:
-                results.reverse()
-
-            results = results[abs(drop):]
-
-        text = f'{person} rolled a total of: {sum(results)}'
-        await message.channel.send(text)
-
-    if contents.startswith('!stat_gen'):
-        text = stat_gen(message)
-        await message.channel.send(text)
-
-    if contents.startswith('!str'):
-        words = contents.split(' ')
-        char_name = ' '.join(words[1:-1])
-        stat = int(words[-1])
-        char_index = character_names.index(char_name)
-        characters[char_index].set_stat('str', stat)
-        bonus = characters[char_index].stat_bonus[0]
-        if bonus < 0:
-            sign = ''
-        else:
-            sign = '+'
-
-        await message.channel.send(f'{char_name} has strength {sign}{bonus}({stat})')
-
-    if contents.startswith('!dex'):
-        words = contents.split(' ')
-        char_name = ' '.join(words[1:-1])
-        stat = int(words[-1])
-        char_index = character_names.index(char_name)
-        characters[char_index].set_stat('dex', stat)
-        bonus = characters[char_index].stat_bonus[1]
-        if bonus < 0:
-            sign = ''
-        else:
-            sign = '+'
-
-        await message.channel.send(f'{char_name} has dexterity {sign}{bonus}({stat})')
-
-    if contents.startswith('!con'):
-        words = contents.split(' ')
-        char_name = ' '.join(words[1:-1])
-        stat = int(words[-1])
-        char_index = character_names.index(char_name)
-        characters[char_index].set_stat('con', stat)
-        bonus = characters[char_index].stat_bonus[2]
-        if bonus < 0:
-            sign = ''
-        else:
-            sign = '+'
-
-        await message.channel.send(f'{char_name} has constitution  {sign}{bonus}({stat})')
-
-    if contents.startswith('!wis'):
-        words = contents.split(' ')
-        char_name = ' '.join(words[1:-1])
-        stat = int(words[-1])
-        char_index = character_names.index(char_name)
-        characters[char_index].set_stat('wis', stat)
-        bonus = characters[char_index].stat_bonus[4]
-        if bonus < 0:
-            sign = ''
-        else:
-            sign = '+'
-
-        await message.channel.send(f'{char_name} has wisdom {sign}{bonus}({stat})')
-
-    if contents.startswith('!int'):
-        words = contents.split(' ')
-        if words[1:-1]:
-            char_name = ' '.join(words[1:-1])
-        else:
-            char_name = ' '.join(words[1:])
-
-        char_index = character_names.index(char_name)
-        if words[-1].isalpha():
-            stat = characters[char_index].stats[3]
-            bonus = characters[char_index].stat_bonus[3]
-
-            if bonus < 0:
-                sign = ''
-            else:
-                sign = '+'
-        else:
-            stat = int(words[-1])
-            characters[char_index].set_stat('int', stat)
-            bonus = characters[char_index].stat_bonus[3]
-            if bonus < 0:
-                sign = ''
-            else:
-                sign = '+'
-
-        await message.channel.send(f'{char_name} has intelegence {sign}{bonus}({stat})')
-
-    if contents.startswith('!cha'):
-        words = contents.split(' ')
-        char_name = ' '.join(words[1:-1])
-        stat = int(words[-1])
-        char_index = character_names.index(char_name)
-        characters[char_index].set_stat('cha', stat)
-        bonus = characters[char_index].stat_bonus[5]
-        if bonus < 0:
-            sign = ''
-        else:
-            sign = '+'
-
-        await message.channel.send(f'{char_name} has charisma {sign}{bonus}({stat})')
-#
-#
-#
-#
-#
-#
-#
+@bot.command(name='hello')
+async def hello(ctx):
+    print("help me!")
+    await ctx.send('Hello!')
 
 
-def new_character(message):
+@bot.command(name='new_character')
+async def new_character(ctx):
+    message = ctx.message
     person = message.author
     chr_name = ' '.join(message.content.split(' ')[1:])
 
@@ -188,10 +42,44 @@ def new_character(message):
     else:
         text = f'A character named {chr_name} already exists. If you wish to create a new character type "!kill {chr_name}" and then try again.'
 
-    return text
+    await ctx.send(text)
 
 
-def stat_gen(message):
+@bot.command(name='roll')
+async def roll(ctx):
+    message = ctx.message
+    person = message.author
+    words = message.content.lower().split(' ')
+
+    multiplicity = int(words[1].split('d')[0])
+    die = int(words[1].split('d')[1])
+    d_max = max(die, 1)
+    d_min = min(die, 1)
+
+    results = []
+
+    await ctx.send(f'Rolling {words[1]}')
+
+    for i in range(multiplicity):
+        roll = random.randint(d_min, d_max)
+        results.append(roll)
+        await ctx.send(f'{roll}')
+        time.sleep(2.4)
+
+    if message.content.count('drop') == 1:
+        drop = int(message.content.split(' ')[-1])
+        results.sort()
+        if drop < 0:
+            results.reverse()
+
+        results = results[abs(drop):]
+
+    text = f'{person} rolled a total of: {sum(results)}'
+    await ctx.send(text)
+
+
+@bot.command(name='stat_gen')
+async def stat_gen(ctx):
     all_stats = []
     for _ in range(6):
         stat = []
@@ -200,7 +88,198 @@ def stat_gen(message):
         stat.sort()
         all_stats.append(sum(stat[1:]))
 
-    return f'Stats are {all_stats}'
+    await ctx.send(f'Stats are {all_stats}')
 
 
-client.run(MY_TOKEN)
+@bot.command(name='str')
+async def strength(ctx):
+    message = ctx.message
+    contents = message.content
+    words = contents.split(' ')
+    if words[1:-1]:
+        char_name = ' '.join(words[1:-1])
+    else:
+        char_name = ' '.join(words[1:])
+
+    char_index = character_names.index(char_name)
+    if words[-1].isalpha():
+        stat = characters[char_index].stats[0]
+        bonus = characters[char_index].stat_bonus[0]
+
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+    else:
+        stat = int(words[-1])
+        characters[char_index].set_stat('str', stat)
+        bonus = characters[char_index].stat_bonus[0]
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+
+    await ctx.send(f'{char_name} has strength {sign}{bonus}({stat})')
+
+
+@bot.command(name='dex')
+async def dexterity(ctx):
+    message = ctx.message
+    contents = message.content
+    words = contents.split(' ')
+    if words[1:-1]:
+        char_name = ' '.join(words[1:-1])
+    else:
+        char_name = ' '.join(words[1:])
+
+    char_index = character_names.index(char_name)
+    if words[-1].isalpha():
+        stat = characters[char_index].stats[1]
+        bonus = characters[char_index].stat_bonus[1]
+
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+    else:
+        stat = int(words[-1])
+        characters[char_index].set_stat('dex', stat)
+        bonus = characters[char_index].stat_bonus[1]
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+
+    await ctx.send(f'{char_name} has dexterity {sign}{bonus}({stat})')
+
+
+@bot.command(name='con')
+async def consitution(ctx):
+    message = ctx.message
+    contents = message.content
+    words = contents.split(' ')
+    if words[1:-1]:
+        char_name = ' '.join(words[1:-1])
+    else:
+        char_name = ' '.join(words[1:])
+
+    char_index = character_names.index(char_name)
+    if words[-1].isalpha():
+        stat = characters[char_index].stats[2]
+        bonus = characters[char_index].stat_bonus[2]
+
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+    else:
+        stat = int(words[-1])
+        characters[char_index].set_stat('con', stat)
+        bonus = characters[char_index].stat_bonus[2]
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+
+    await ctx.send(f'{char_name} has consitution {sign}{bonus}({stat})')
+
+
+@bot.command(name='int')
+async def intelegence(ctx):
+    message = ctx.message
+    contents = message.content
+    words = contents.split(' ')
+    if words[1:-1]:
+        char_name = ' '.join(words[1:-1])
+    else:
+        char_name = ' '.join(words[1:])
+
+    char_index = character_names.index(char_name)
+    if words[-1].isalpha():
+        stat = characters[char_index].stats[3]
+        bonus = characters[char_index].stat_bonus[3]
+
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+    else:
+        stat = int(words[-1])
+        characters[char_index].set_stat('int', stat)
+        bonus = characters[char_index].stat_bonus[3]
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+
+    await ctx.send(f'{char_name} has intelegence {sign}{bonus}({stat})')
+
+
+@bot.command(name='wis')
+async def wisdom(ctx):
+    message = ctx.message
+    contents = message.content
+    words = contents.split(' ')
+    if words[1:-1]:
+        char_name = ' '.join(words[1:-1])
+    else:
+        char_name = ' '.join(words[1:])
+
+    char_index = character_names.index(char_name)
+    if words[-1].isalpha():
+        stat = characters[char_index].stats[4]
+        bonus = characters[char_index].stat_bonus[4]
+
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+    else:
+        stat = int(words[-1])
+        characters[char_index].set_stat('wis', stat)
+        bonus = characters[char_index].stat_bonus[4]
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+
+    await ctx.send(f'{char_name} has wisdom {sign}{bonus}({stat})')
+
+
+@bot.command(name='cha')
+async def charisma(ctx):
+    message = ctx.message
+    contents = message.content
+    words = contents.split(' ')
+    if words[1:-1]:
+        char_name = ' '.join(words[1:-1])
+    else:
+        char_name = ' '.join(words[1:])
+
+    char_index = character_names.index(char_name)
+    if words[-1].isalpha():
+        stat = characters[char_index].stats[5]
+        bonus = characters[char_index].stat_bonus[5]
+
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+    else:
+        stat = int(words[-1])
+        characters[char_index].set_stat('cha', stat)
+        bonus = characters[char_index].stat_bonus[5]
+        if bonus < 0:
+            sign = ''
+        else:
+            sign = '+'
+
+    await ctx.send(f'{char_name} has charisma {sign}{bonus}({stat})')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+bot.run(MY_TOKEN)
